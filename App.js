dab1ff47f34f75d4635a6bd8db39491d6d992b96
Vivek Mandal <vivek.mandal@shipyaari.com>
2023-09-11 23:51:25 +0530

    chapter 2

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement('div', {id:'parent'}, [
   React.createElement('div', {id:'child'}, [
-    React.createElement('h1', {}, 'Hello World!'),
-    React.createElement('h2', {}, 'Hello World!')
+    React.createElement('h1', {}, 'Namaskaram React 🚀'),
+    React.createElement('h2', {}, 'Hello test!')
   ]),
   React.createElement('div', {id:'child2'}, [
     React.createElement('h1', {}, 'Hello World!'),
---
+++ README.md
@@ -0,0 +1,14 @@
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorothm - Written in C++
+- Caching - Faster Build
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - to support older browsers
+- Tree Shaking - remove unused code
