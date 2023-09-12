ee1f996f5d3134bbc37ad40bfc0db0191926486a
Vivek Mandal <vivek.mandal@shipyaari.com>
2023-09-12 23:58:48 +0530

    chapter 3

--- App.js
+++ App.js
@@ -1,18 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement('div', {id:'parent'}, [
-  React.createElement('div', {id:'child'}, [
-    React.createElement('h1', {}, 'Namaskaram React 🚀'),
-    React.createElement('h2', {}, 'Hello test!')
-  ]),
-  React.createElement('div', {id:'child2'}, [
-    React.createElement('h1', {}, 'Hello World!'),
-    React.createElement('h2', {}, 'Hello World!')
-  ])
-]);
-
-// console.log(heading);
+const jsxHeading = <h1 id="heading">Namaskaram React using JSX 🚀</h1>;
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+
+root.render(jsxHeading);
