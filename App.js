08adec11ca07fc526430438b0fe16b4dc9bc004a
Vivek Mandal <vivek.mandal@shipyaari.com>
2023-09-15 00:50:16 +0530

    chapter 4

--- App.js
+++ App.js
@@ -1,22 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => ( 
-  <h1 className="heading" tabIndex={5}>
-    Namaskaram React using JSX 🚀
-  </h1>
-);
+/*
+* Header
+* - Logo
+* - Nav Items
+* Body
+* - Search
+* - Restaurant Container
+*   - Restaurant Card
+*     - Restaurant Name, Rating
+*     - Dish Name, Price
+*     - Time
+* Footer
+* - Copyright
+* - Links 
+* - Address
+* - Contact
+*/
 
-// React functinal component
-const HeadingComponent = () => (
-  <div id='container'>
-    {Title()}
-    <Title />
-    <Title></Title>
-    <h1>Namaskaram React functional component</h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img 
+          className="logo"
+          src="https://www.clipartmax.com/png/small/112-1129793_healthy-food-logo-png.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+};
+
+const RestaurantCard = () => {
+  return (
+    <div>
+      <h2 className="restaurant-name">Restaurant Name</h2>
+      <div className="rating">Rating</div>
+    </div>
+    // <div>
+    //   <p className="dish-name">Dish Name</p>
+    // </div>
+  )
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search </div>
+      <div className="restaurant-container">
+        <RestaurantCard />
+      </div>
+    </div>
+  )
+};
+
+const AppLayout = () => {
+  return <div className="App">
+    <Header />
+    <Body />
   </div>
-);
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
+
