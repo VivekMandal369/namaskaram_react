5a6417dfe9733cb45ababfdb79aa25df4f8cc5f9
Mr. Zero <vivek248.vm@gmail.com>
2024-01-27 05:50:21 +0530

    restaurant card added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+
 /*
 * Header
 * - Logo
@@ -19,13 +20,12 @@ import ReactDOM from "react-dom/client";
 * - Contact
 */
 
-const Header = () => {
-  return (
+const Header = () => (
     <div className="header">
       <div className="logo-container">
         <img 
           className="logo"
-          src="https://www.clipartmax.com/png/small/112-1129793_healthy-food-logo-png.png"
+          src={require('./Images/Annam.gif')}
         />
       </div>
       <div className="nav-items">
@@ -37,38 +37,713 @@ const Header = () => {
         </ul>
       </div>
     </div>
-  )
+);
+
+const RestaurantCard = (props) => {
+  const {cardData} = props;
+  const {name, cloudinaryImageId, avgRating, sla, cuisines, costForTwo} = cardData?.info;
+  const {slaString} = sla;
+  return (
+    <div className="res-card">
+      <img className="food-img" src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}></img>
+      <h2 className="restaurant-name">{name}</h2>
+      <div className="restaurant-name">
+        <span className="cardleft rating">⭐ {avgRating}</span>
+        <span className="cardright">{slaString}</span>
+      </div>
+      <div className="cuisinesprice">
+        <span className="cardleft">{cuisines.join(', ')}</span>
+        <span className="cardright">{costForTwo}</span>
+      </div>
+    </div>
+  );
 };
 
-const RestaurantCard = () => {
-  return (
-    <div>
-      <h2 className="restaurant-name">Restaurant Name</h2>
-      <div className="rating">Rating</div>
-    </div>
-    // <div>
-    //   <p className="dish-name">Dish Name</p>
-    // </div>
-  )
-};
+const data = [
+  {
+  "info": {
+  "id": "117397",
+  "name": "La Pino'z Pizza",
+  "cloudinaryImageId": "pft1dcdx8udfepmcnuty",
+  "locality": "Sion",
+  "areaName": "Sion",
+  "costForTwo": "₹550 for two",
+  "cuisines": [
+  "Pizzas",
+  "Pastas",
+  "Italian",
+  "Desserts",
+  "Beverages"
+  ],
+  "avgRating": 4.3,
+  "parentId": "4961",
+  "avgRatingString": "4.3",
+  "totalRatingsString": "10K+",
+  "sla": {
+  "deliveryTime": 26,
+  "lastMileTravel": 4.2,
+  "serviceability": "SERVICEABLE",
+  "slaString": "25-30 mins",
+  "lastMileTravelString": "4.2 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-03 00:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "50% OFF",
+  "subHeader": "UPTO ₹100"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-bd33cf88-5b18-4e5d-815d-5702b531fafe"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/la-pinoz-pizza-sion-mumbai-117397",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "233331",
+  "name": "Burger King",
+  "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+  "locality": "SG Barve Marg",
+  "areaName": "Kurla",
+  "costForTwo": "₹350 for two",
+  "cuisines": [
+  "Burgers",
+  "American"
+  ],
+  "avgRating": 4.2,
+  "parentId": "166",
+  "avgRatingString": "4.2",
+  "totalRatingsString": "5K+",
+  "sla": {
+  "deliveryTime": 27,
+  "lastMileTravel": 3,
+  "serviceability": "SERVICEABLE",
+  "slaString": "25-30 mins",
+  "lastMileTravelString": "3.0 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-01-27 06:59:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "60% OFF",
+  "subHeader": "UPTO ₹120"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-bd33cf88-5b18-4e5d-815d-5702b531fafe"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/burger-king-sg-barve-marg-kurla-mumbai-233331",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "649531",
+  "name": "Kwality Walls Frozen Dessert and Ice Cream Shop",
+  "cloudinaryImageId": "909daf097e30cd01d8fd121ddc6de2f6",
+  "locality": "Tilak road",
+  "areaName": "Ghatkopar Vikhroli",
+  "costForTwo": "₹200 for two",
+  "cuisines": [
+  "Desserts",
+  "Ice Cream",
+  "Ice Cream Cakes"
+  ],
+  "avgRating": 4.6,
+  "veg": true,
+  "parentId": "582",
+  "avgRatingString": "4.6",
+  "totalRatingsString": "100+",
+  "sla": {
+  "deliveryTime": 25,
+  "lastMileTravel": 4.4,
+  "serviceability": "SERVICEABLE",
+  "slaString": "20-25 mins",
+  "lastMileTravelString": "4.4 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-02-03 00:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "₹150 OFF",
+  "subHeader": "ABOVE ₹349",
+  "discountTag": "FLAT DEAL"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-bd33cf88-5b18-4e5d-815d-5702b531fafe"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/kwality-walls-frozen-dessert-and-ice-cream-shop-tilak-road-ghatkopar-vikhroli-mumbai-649531",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "55019",
+  "name": "Baskin Robbins - Ice Cream Desserts",
+  "cloudinaryImageId": "85ccae4e3576f9330af102c46ca85395",
+  "locality": "Chetna College Road",
+  "areaName": "Bandra East",
+  "costForTwo": "₹350 for two",
+  "cuisines": [
+  "Desserts",
+  "Ice Cream"
+  ],
+  "avgRating": 4.6,
+  "veg": true,
+  "parentId": "5588",
+  "avgRatingString": "4.6",
+  "totalRatingsString": "1K+",
+  "sla": {
+  "deliveryTime": 23,
+  "lastMileTravel": 4,
+  "serviceability": "SERVICEABLE",
+  "slaString": "20-25 mins",
+  "lastMileTravelString": "4.0 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-01-27 04:00:00",
+  "opened": true
+  },
+  "badges": {
+  "textExtendedBadges": [
+  {
+  "iconId": "v1705582451/Ratnesh_Badges/Listing_HR.png",
+  "shortDescription": "Perfect Cake Delivery",
+  "fontColor": "#7E808C"
+  }
+  ]
+  },
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {
+  "badgeObject": [
+  {
+  "attributes": {
+  "description": "",
+  "fontColor": "#7E808C",
+  "iconId": "v1705582451/Ratnesh_Badges/Listing_HR.png",
+  "shortDescription": "Perfect Cake Delivery"
+  }
+  }
+  ]
+  }
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "30% OFF",
+  "subHeader": "UPTO ₹75"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-bd33cf88-5b18-4e5d-815d-5702b531fafe"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/baskin-robbins-ice-cream-desserts-chetna-college-road-bandra-east-mumbai-55019",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "802336",
+  "name": "Yasin's Food Inn",
+  "cloudinaryImageId": "ad71e7632fb460a2559b64289ec9c2b2",
+  "locality": "Radhabai Niwas Corner",
+  "areaName": "Kurla",
+  "costForTwo": "₹300 for two",
+  "cuisines": [
+  "North Indian",
+  "Chinese",
+  "Mughlai",
+  "Fast Food",
+  "Biryani",
+  "Beverages"
+  ],
+  "avgRating": 3.7,
+  "parentId": "313916",
+  "avgRatingString": "3.7",
+  "totalRatingsString": "20+",
+  "sla": {
+  "deliveryTime": 25,
+  "lastMileTravel": 1,
+  "serviceability": "SERVICEABLE",
+  "slaString": "25-30 mins",
+  "lastMileTravelString": "1.0 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-01-27 04:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "60% OFF",
+  "subHeader": "UPTO ₹120"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "isNewlyOnboarded": true,
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-bd33cf88-5b18-4e5d-815d-5702b531fafe"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/yasins-food-inn-radhabai-niwas-corner-kurla-mumbai-802336",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "106763",
+  "name": "Rangoon Zaika",
+  "cloudinaryImageId": "o5bbufmcjoqdfvtm7r1n",
+  "locality": "Kurla",
+  "areaName": "Kurla West",
+  "costForTwo": "₹400 for two",
+  "cuisines": [
+  "Indian",
+  "Chinese"
+  ],
+  "avgRating": 3.9,
+  "parentId": "166932",
+  "avgRatingString": "3.9",
+  "totalRatingsString": "5K+",
+  "sla": {
+  "deliveryTime": 23,
+  "lastMileTravel": 0.6,
+  "serviceability": "SERVICEABLE",
+  "slaString": "20-25 mins",
+  "lastMileTravelString": "0.6 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-01-27 05:30:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "ITEMS",
+  "subHeader": "AT ₹299"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-bd33cf88-5b18-4e5d-815d-5702b531fafe"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/rangoon-zaika-kurla-kurla-west-mumbai-106763",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "6663",
+  "name": "Mughal Dhaba",
+  "cloudinaryImageId": "df600a304a4c328795e659b072d93208",
+  "locality": "LBS Marg, Kurla West",
+  "areaName": "Kurla West",
+  "costForTwo": "₹500 for two",
+  "cuisines": [
+  "Mughlai",
+  "North Indian",
+  "Chinese"
+  ],
+  "avgRating": 4.2,
+  "parentId": "140929",
+  "avgRatingString": "4.2",
+  "totalRatingsString": "1K+",
+  "sla": {
+  "deliveryTime": 23,
+  "lastMileTravel": 0.6,
+  "serviceability": "SERVICEABLE",
+  "slaString": "20-25 mins",
+  "lastMileTravelString": "0.6 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-01-27 04:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "ITEMS",
+  "subHeader": "AT ₹249"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-bd33cf88-5b18-4e5d-815d-5702b531fafe"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/mughal-dhaba-lbs-marg-kurla-west-mumbai-6663",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "111518",
+  "name": "Kurla Juicy Hut",
+  "cloudinaryImageId": "s6fhwzl0tss0vgrqvcid",
+  "locality": "Kurla",
+  "areaName": "Kurla West",
+  "costForTwo": "₹300 for two",
+  "cuisines": [
+  "Beverages",
+  "Desserts"
+  ],
+  "avgRating": 4.2,
+  "parentId": "121989",
+  "avgRatingString": "4.2",
+  "totalRatingsString": "5K+",
+  "sla": {
+  "deliveryTime": 19,
+  "lastMileTravel": 0.6,
+  "serviceability": "SERVICEABLE",
+  "slaString": "15-20 mins",
+  "lastMileTravelString": "0.6 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-01-27 05:00:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "aggregatedDiscountInfoV3": {
+  "header": "10% OFF",
+  "subHeader": "ABOVE ₹700",
+  "discountTag": "FLAT DEAL"
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-bd33cf88-5b18-4e5d-815d-5702b531fafe"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/kurla-juicy-hut-kurla-kurla-west-mumbai-111518",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  },
+  {
+  "info": {
+  "id": "632822",
+  "name": "Maajis Foods 2.0",
+  "cloudinaryImageId": "m7xvnywikzvpubucdp0b",
+  "locality": "Chapel Rd",
+  "areaName": "Bandra West",
+  "costForTwo": "₹120 for two",
+  "cuisines": [
+  "Chinese",
+  "Mughlai",
+  "Seafood"
+  ],
+  "avgRating": 3.9,
+  "parentId": "379290",
+  "avgRatingString": "3.9",
+  "totalRatingsString": "100+",
+  "sla": {
+  "deliveryTime": 36,
+  "lastMileTravel": 5.8,
+  "serviceability": "SERVICEABLE",
+  "slaString": "35-40 mins",
+  "lastMileTravelString": "5.8 km",
+  "iconType": "ICON_TYPE_EMPTY"
+  },
+  "availability": {
+  "nextCloseTime": "2024-01-27 04:04:00",
+  "opened": true
+  },
+  "badges": {},
+  "isOpen": true,
+  "aggregatedDiscountInfoV2": {},
+  "type": "F",
+  "badgesV2": {
+  "entityBadges": {
+  "imageBased": {},
+  "textBased": {},
+  "textExtendedBadges": {}
+  }
+  },
+  "orderabilityCommunication": {
+  "title": {},
+  "subTitle": {},
+  "message": {},
+  "customIcon": {}
+  },
+  "differentiatedUi": {
+  "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+  "differentiatedUiMediaDetails": {
+  "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+  "lottie": {},
+  "video": {}
+  }
+  },
+  "reviewsSummary": {},
+  "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+  "restaurantOfferPresentationInfo": {}
+  },
+  "analytics": {
+  "context": "seo-data-bd33cf88-5b18-4e5d-815d-5702b531fafe"
+  },
+  "cta": {
+  "link": "https://www.swiggy.com/restaurants/maajis-foods-2-0-chapel-rd-bandra-west-mumbai-632822",
+  "text": "RESTAURANT_MENU",
+  "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  }
+  ];
 
 const Body = () => {
   return (
     <div className="body">
       <div className="search">Search </div>
       <div className="restaurant-container">
-        <RestaurantCard />
+        {
+          data.map(restaurant => <RestaurantCard key={restaurant.info.id} cardData={restaurant}/>)
+        }
+        {/* <RestaurantCard cardData={data[0]} /> */}
+        {/* <RestaurantCard cardData={data[1]} /> */}
       </div>
     </div>
   )
 };
 
-const AppLayout = () => {
-  return <div className="App">
+const AppLayout = () => (
+   <div className="app">
     <Header />
     <Body />
   </div>
-};
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
